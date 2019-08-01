e9554c39eb79d3340bbe15eb528b61046e58483f
Decosan <junx0401@gmail.com>
2019-08-01 20:43:42 +0900

    2nd

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,4 +1,6 @@
 class LinebotController < ApplicationController
+  require 'line/bot'
+
   protect_from_forgery :except => [:callback]
 
   def client
@@ -18,6 +20,47 @@ class LinebotController < ApplicationController
 
     events = client.parse_events_from(body)
 
-    # LINE Botがメッセージを受け取ってどんな処理をするのか、ご自身で調べて追記してみましょう！
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
+          if event.message['text'].eql?('アンケート')
+            # private内のtemplateメソッドを呼び出します。
+            client.reply_message(event['replyToken'], template)
+          end
+        end
+      end
+    }
+
+    head :ok
+  end
+
+  private
+
+  def template
+    {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": "今日のもくもく会は楽しいですか？",
+          "actions": [
+              {
+                "type": "message",
+                # Botから送られてきたメッセージに表示される文字列です。
+                "label": "楽しい",
+                # ボタンを押した時にBotに送られる文字列です。
+                "text": "楽しい"
+              },
+              {
+                "type": "message",
+                "label": "楽しくない",
+                "text": "楽しくない"
+              }
+          ]
+      }
+    }
   end
 end
